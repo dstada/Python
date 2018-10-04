5f58b99ceb5d442b95f9bf44aafd8a0e54b32e4b
dstada <dickstada@gmail.com>
2018-10-04 21:09:43 +0200

    finished

--- owl_check.py
+++ owl_check.py
@@ -1,8 +1,17 @@
+'''Owl challenge Sololearn.com
+   Asks a matrix of numbers
+   and checks if the matrix can be folded
+   horizontal and vertical.
+   Input must be rules of numbers, separated by commas.
+
+    By: Dick Stada, NL
+    October 2018
+    TODO: input control
+'''
+
+
 def make_matrix():
     print("Give a rule of numbers, separated by a comma")
-    # Voeg input toe aan array
-    # Vraag of er nog een regel bij moet
-    # Ontvang de extra input
     matrix_array = []
     next_line = "j"
     while next_line == "j" or next_line == "J":
@@ -15,34 +24,25 @@ def make_matrix():
 
 
 def check_matrix(mat_arr):
-    print(mat_arr)
-    # Tel aantal regels
-    print("Aantal cijfers horizontaal: {}".format(len(mat_arr[0])))
-    # Eerst checken of elke regel symmetrisch is
+    # Check each rule on symmetricity
     hor_flag = True
-    for i in mat_arr:   # voor elke regel:
-        if i != i[::-1]:
+    for i in mat_arr:   # for each rule:
+        if i != i[::-1]:  # rule and reversed rule must be identical
             hor_flag = False
     print(hor_flag)
-    # Nu checken of het verticaal symetrisch is
-    print("Aantal regels (verticaal): {}".format(len(mat_arr)))
-    nbr_rules = len(mat_arr)
-    # Vergelijk eerste en laatste regel:
-    print("Laatste regel: {}".format(mat_arr[nbr_rules - 1]))
-    print("Eerste regel: {}".format(mat_arr[nbr_rules - nbr_rules]))
-    # aantal keer dat er regels moeten worden vergeleken:
-    # Aantal regels / 2 (bij oneven hoeft tussenregel niet te worden gecheckt)
-    checks = nbr_rules // 2
-    print(checks)
+    # Check vertical symmetry
+    nbr_rules: int = len(mat_arr)
+    checks = nbr_rules // 2     # with odd number of rules, central rule not checked
     ver_flag = True
     for y in range(0, checks):
-        print("Check")
         # Compare the two rules:
-        print("Nu kijken naar regel {} en {}.".format(y, nbr_rules - y - 1))
         if mat_arr[y] != mat_arr[nbr_rules - y - 1]:
-            print("Vert.check not okay!")
             ver_flag = False
     print(ver_flag)
+    if hor_flag True and ver_flag True:
+        print("This matrix is owl!")
+    else:
+        print("This matrix is unfortunately no owl...")
 
 
 if __name__ == "__main__":

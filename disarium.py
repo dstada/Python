fa5cbb4789681beefe195864334ecd3948370857
dstada <dickstada@gmail.com>
2018-10-08 12:55:08 +0200

    nieuwe aanpassingen

--- disarium.py
+++ disarium.py
@@ -6,11 +6,15 @@ Input: 135
 Output: True
 135 is a Disarium number because it equals to 1^1 + 3^2 + 5^3
 
+Bonus: Print all disarium numbers in a given range.
+
 By: Dick Stada, NL
 October 2018
 """
-
-nmbr = int(input("Give a number:"))
+nmbr_from = int(input("Give the number to  start with:"))
+nmbr_upto = nmbr_from
+while nmbr_upto <= nmbr_from:   # nmbr_upto must be equal or greater
+    nmbr_upto = int(input("Give a maximum number:"))
 
 
 def check_disarium(num):
@@ -26,6 +30,11 @@ def check_disarium(num):
 
 
 if __name__ == "__main__":
-    for x in range(0, nmbr+1):
+    disariums = 0
+    for x in range(nmbr_from, nmbr_upto + 1):
         if check_disarium(x):
             print("{} is a disarium number.".format(x))
+            disariums += 1
+    if disariums == 0:
+        print("No numbers that are a disarium number in this range.")
+

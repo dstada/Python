7d4a7884e2aad182dc06d99f040771dcf9fbded7
dstada <dickstada@gmail.com>
2018-10-08 10:33:31 +0200

    nieuwe aanpassingen

--- disarium.py
+++ disarium.py
@@ -1,12 +1,13 @@
-"""
-A number is called a Disarium number if the sum of the powers of its digits
+"""A number is called a Disarium number if the sum of the powers of its digits
 equals the number itself. The digits are powered to their positions in the number.
 
 For example:
 Input: 135
-Output: true
+Output: True
 135 is a Disarium number because it equals to 1^1 + 3^2 + 5^3
-(each digit powered to the position in the number).
+
+By: Dick Stada, NL
+October 2018
 """
 
 nmbr = int(input("Give a number:"))
@@ -24,6 +25,7 @@ def check_disarium(num):
     return False
 
 
-for x in range(0, nmbr+1):
-    if check_disarium(x):
-        print("{} is a disarium number.".format(x))
+if __name__ == "__main__":
+    for x in range(0, nmbr+1):
+        if check_disarium(x):
+            print("{} is a disarium number.".format(x))

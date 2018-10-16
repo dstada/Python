a7d8c46695cde7886cbbdb0885a2be3390241136
dstada <dickstada@gmail.com>
2018-10-16 14:14:14 +0200

    nieuwe aanpassingen

--- finger_counting.py
+++ finger_counting.py
@@ -54,3 +54,17 @@ Happy Coding!!!
 
 
 '''
+
+nmbr = int(input("Give number: "))
+print(nmbr)
+hand = ["t", "i", "m", "r", "b", "t_r", "i_r", "r_r", "m_r", "b_r"]
+rev_hand = list(reversed(hand))
+print(hand)
+print(rev_hand)
+'''
+1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25	26	27	28	29	30	31	32	33	34	35	36	37
+d	w	m	r	p	dr	wr	mr	rr	pr																											
+									pr	rr	mr	wr	dr	p	r	m	w	d																		
+																		d	w	m	r	p	dr	wr	mr	rr	pr									
+																											pr	rr	mr	wr	dr	p	r	m	w	d
+'''

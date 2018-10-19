8cf2e6bdc7f4066e33a6313941a63051ff5b17b0
dstada <dickstada@gmail.com>
2018-10-19 13:38:40 +0200

    nieuwe aanpassingen

--- guitar_subset.py
+++ guitar_subset.py
@@ -15,5 +15,31 @@ Integers can appear more than once in the list. You may assume all numbers in th
 Write a program to check if the user input has a guitar subset for the specified number G
 or not (both the list of integers and the number G are input parameters).
 """
-
+from itertools import combinations
 nmbr = int(input("Give a number:"))
+# S = []
+lst_str = input("Give a list of integers, separated by a space")
+print(lst_str)
+# Make list of the numbers in lst_str:
+lst = lst_str.split()       # Make a list of the npu string
+print(lst)
+# Find out all combinations of the elements of lst:
+
+
+from itertools import combinations as comb
+g = int(input())
+l = list(input().split(","))
+for i in range(len(l)):
+    l[int(i)] = int(l[int(i)])
+def solve():
+    for i in range(len(l)):
+        for c in comb(l,i+1):
+            if sum(c) == g:
+                yield c
+if solve() == None:
+    print("No subsets found.")
+    exit()
+print("Target number:",g,"\nInitial set:",l,"\nGuitar subset(s):",list(solve()))
+
+if __name__ == "__main__":
+    print("Einde")

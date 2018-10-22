8dcfa7bcd13957a03a7aad2aee6638771e547e4c
Dick Stada <dickstada@gmail.com>
2018-10-22 22:29:11 +0200

    finished

--- linear_equation_ds.py
+++ linear_equation_ds.py
@@ -4,11 +4,19 @@ print(eq)
 
 # Split the equation on the equal sign:
 eq_left = eq.split("=")[0]
-eq_right = eq.split("=")[1]
+eq_right = float(eq.split("=")[1])
 print("Links: {} and right: {}".format(eq_left, eq_right))
 
 # Split the part with the x (aangenomen dat x altijd in links zit):
 eq_left_left = float(eq_left.split("x")[0])
 print(eq_left_left)
+eq_left_right = float(eq_left.split("x")[1])
+print(eq_left_right)
+# Left number to the right:
+eq_right = eq_right - eq_left_right
+print(eq_right)
+# Get x by dividing right by left:
+eq_right = eq_right / eq_left_left
+print(eq_right)
 
 # https://code.sololearn.com/cZJeTyhTIexH/#py

966b17cecaea041fab5fef0cb1094f4552523728
dstada <dickstada@gmail.com>
2018-11-01 14:15:41 +0100

    nieuwe aanpassingen

--- monday.py
+++ monday.py
@@ -16,22 +16,27 @@ October 2018
 """
 import datetime
 import calendar
+from dateutil import parser
 
 datetime.datetime.today()
 # datetime.datetime(2012, 3, 23, 23, 24, 55, 173504)
 datetime.datetime(2012, 3, 23)
 print(datetime.datetime.today().weekday())
 
+# ================================================
 dt = '21/03/2012'
 day, month, year = (int(x) for x in dt.split('/'))
 ans = datetime.date(year, month, day)
 print(ans.strftime("%A"))
+# ================================================
 
-from dateutil import parser
+print("start")
 print(parser.parse('January 11, 2010').strftime("%A"))
-
 print(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][calendar.weekday(2017, 12, 22)])
+print("einde")
 
+
+print("===========================================")
 print("Give your day and choose your format:")
 dt = input("Give date: ")
 cmd = None
@@ -40,8 +45,6 @@ while cmd != 'x':
                 '[f] 31-12-2018\n[g] 12-31-2018')
     cmd = cmd.lower().strip()
     if cmd == 'a':
-        print("Choice is a")
-        form = "a"
         day, month, year = (int(x) for x in dt.split('/'))
         ans = datetime.date(year, month, day)
         print(ans.strftime("%A"))
@@ -51,27 +54,39 @@ while cmd != 'x':
         form = "b"
         break
     elif cmd == 'c':
-        print("Choice is c")
-        form = "c"
+        year, day, month = (int(x) for x in dt.split(','))
+        ans = datetime.date(year, month, day)
+        print(ans.strftime("%A"))
         break
-    elif cmd == "d":
-        print("Choice is d")
-        form = "d"
-    elif cmd == "e":
-        print("Choice is e")
-        form = "e"
+    elif cmd == "d":    # 20181231
+        year = int(dt[:4])
+        print(year)
+        month = int(dt[4:6])
+        print(month)
+        day = int(dt[6:])
+        print(day)
+        ans = datetime.date(year, month, day)
+        print(ans.strftime("%A"))
+        break
+    elif cmd == "e":    # 31122018
+        year = int(dt[4:])
+        print(year)
+        month = int(dt[2:4])
+        print(month)
+        day = int(dt[0:2])
+        print(day)
+        ans = datetime.date(year, month, day)
+        print(ans.strftime("%A"))
+        break
     elif cmd == "f":
-        print("Choice is f")
-        form = "f"
         day, month, year = (int(x) for x in dt.split('-'))
         ans = datetime.date(year, month, day)
         print(ans.strftime("%A"))
         break
     elif cmd == "g":
-        print("Choice is g")
-        form = "g"
+        month, day, year = (int(x) for x in dt.split('-'))
+        ans = datetime.date(year, month, day)
+        print(ans.strftime("%A"))
+        break
     elif cmd != 'x':
         print("Sorry, we don't understand {}".format(cmd))
-
-
-
